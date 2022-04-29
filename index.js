27dd796320f127c5248689eaade26d209e70914c
hoonkim <hoonk9387@gmail.com>
2022-04-28 22:01:29 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+
+    const input = document.getElementById('input')
+    // console.log(input)
+    input.addEventListener('click', function() {
+        alert('I was clicked!!!')
+    })
 }
+
+addingEventListener()
